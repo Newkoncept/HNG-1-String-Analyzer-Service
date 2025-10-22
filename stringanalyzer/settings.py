46fc0ce5d7f1650851ddc2d0699226891fe2faec
Oluwagbemiga <taiwoemmanuel15@gmail.com>
2025-10-22 19:27:22 +0100

    Change allow host in settings.py file

--- stringanalyzer/settings.py
+++ stringanalyzer/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["hng-1-string-analyzer-service-production.up.railway.app"]
 
 
 # Application definition
